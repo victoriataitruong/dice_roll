3cd90a815394ab3c88b63c0434bc11194ff9f2a1
Victoria Truong <victoriataitruong@gmail.com>
2021-06-13 14:25:29 -0700

    updated comments

--- dice_roll.py
+++ dice_roll.py
@@ -1,13 +1,14 @@
+# libraries imported
 import tkinter
 from PIL import Image, ImageTk
 import random
 
-# toplevel widget which represents the main window of an application
+# top of the widget
 root = tkinter.Tk()
 root.geometry('400x400')
 root.title('Roll the Dice')
 
-# Adding label into the frame
+# adding label into the frame
 l0 = tkinter.Label(root, text="")
 l0.pack()
 
@@ -19,6 +20,7 @@ l1.pack()
 
 # images
 dice = ['die1.png', 'die2.png', 'die3.png', 'die4.png', 'die5.png', 'die6.png']
+
 # simulating the dice with random numbers between 0 to 6 and generating image
 image1 = ImageTk.PhotoImage(Image.open(random.choice(dice)))
 
